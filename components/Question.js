2a566fa07507ca85d7f8a4bd753909c5c5c39bea
Yanis <66034525+Yan4on@users.noreply.github.com>
2020-12-18 23:06:37 +0300

    Dev quiz (#4)
    * делаю капчу
    
    * добавил капчу
    
    * добавил попап капча
    
    * делаю тестовую кнопку капчи
    
    * делаю тестовую кнопку капчи
    
    * добавил тестовую кнопку капча
    
    Co-authored-by: Shirokikh Yanis <yan4on@yandex.ru>

---
+++ components/Question.js
@@ -0,0 +1,14 @@
+//Класс, представляющий вопрос
+export default class Question 
+{
+	constructor(text, answers)
+	{
+		this.text = text; 
+		this.answers = answers; 
+	}
+
+	Click(index) 
+	{
+		return this.answers[index].value; 
+	}
+}

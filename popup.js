71968f9cc46075628fd94f063b66b0f7c4b8d512
Aleksandra-Shevchenko <shevdorosh@yandex.ru>
2020-12-19 12:38:51 +0300

    Добавлен попап бланка

--- popup.js
+++ popup.js
@@ -2,20 +2,23 @@
 const content = document.querySelector('.content');
 const subcategoryList = content.querySelectorAll('.card__menu-item');
 
-const popupWithForm = document.querySelector('.popup');
-const popupCloseBtn = document.querySelector('.popup__close');
+const popupWithForm = document.querySelector('.popup_type_form');
+const popupForm =popupWithForm.querySelector('.popup__form');
+const inputList = popupForm.querySelectorAll('.popup__input');
+const popupInputCat = popupWithForm.querySelector('#category');
 
 
-
+const popupWithBlank = document.querySelector('.popup_type_blank');
+const popupSendBlank = document.querySelector('.popup__send-btn');
 
 
 // --- ФУНКЦИИ ---
-function openPopup() {
-  popupWithForm.classList.add('popup_opened');
+function openPopup(typePopup) {
+  typePopup.classList.add('popup_opened');
 }
 
-function closePopup() {
-  popupWithForm.classList.remove('popup_opened');
+function closePopup(typePopup) {
+  typePopup.classList.remove('popup_opened');
 }
 
 function handlePopupClick(evt) {
@@ -24,20 +27,59 @@ function handlePopupClick(evt) {
   }
 }
 
+// функция заполнения бланка
+function fillBlank({
+  popupSurname,
+  popupName,
+  popupEmail,
+  popupTel,
+  popupCategory
+ }) {
+  popupWithBlank.querySelector('.popup__name').textContent = `${popupSurname} ${popupName}`;
+  popupWithBlank.querySelector('.popup__email').textContent = `e-mail: ${popupEmail}`;
+  popupWithBlank.querySelector('.popup__tel').textContent = `тел.: ${popupTel}`;
+  popupWithBlank.querySelector('.popup__category').textContent = `Вид услуги: ${popupCategory}`;
+}
 
+
+function handlePopupSubmit(evt) {
+  evt.preventDefault();
+  const formValues = {};
+
+  inputList.forEach((input) => {
+    formValues[input.name] = input.value;
+  });
+
+  fillBlank(formValues);
+
+  closePopup(popupWithForm);
+  openPopup(popupWithBlank);
+}
 
 
 
 // --- ОБРАБОТЧИКИ СОБЫТИЙ ---
 subcategoryList.forEach(element => {
-  element.addEventListener('click', openPopup);
+  element.addEventListener('click', () => {
+    openPopup(popupWithForm);
+    popupInputCat.value = element.textContent;
+  });
 });
 
-popupCloseBtn.addEventListener('click', closePopup);
+
+document.querySelectorAll('.popup__close').forEach(element =>{
+  element.addEventListener('click', () => {
+    closePopup(element.closest('.popup'))
+  });
+})
+
 
 popupWithForm.addEventListener('mousedown', handlePopupClick);
 
+popupForm.addEventListener("submit", handlePopupSubmit);
 
-
+popupSendBlank.addEventListener("click", () => {
+  closePopup(popupWithBlank);
+});
 
 
